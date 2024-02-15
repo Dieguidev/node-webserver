171825dcd2f140659a9606a7df2705263147eb9c
Dieguidev <diegogaraycullas@gmail.com>
2024-02-15 12:59:21 -0500

    sirviendo un sitio web completo

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const port = 8080;
 //servir contenido estatico
 app.use(express.static('public'));
 
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html');
+});
+
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/404.html');
 });
