30bd42ce98cd714ba1d4395350f55595d6acfcc3
Dieguidev <diegogaraycullas@gmail.com>
2024-02-15 15:42:01 -0500

    se crearon archivos partials para desplegar app con handlebars

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 const port = 8080;
 
-//todo: require('hbs)
+//handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 //servir contenido estatico
 app.use(express.static('public'));
@@ -16,12 +19,28 @@ app.get('/', (req, res) => {
 });
 
 app.get('/generic', (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html');
+  res.render('generic', {
+    name: 'Dieguidev',
+    title: 'Happy Node',
+    class: 'alt',
+  });
 });
 
+// app.get('/generic', (req, res) => {
+//   res.sendFile(__dirname + '/public/generic.html');
+// });
+
 app.get('/elements', (req, res) => {
-  res.sendFile(__dirname + '/public/elements.html');
+  res.render('elements', {
+    name: 'Dieguidev',
+    title: 'Happy Node',
+    class: 'alt',
+  });
 });
+
+// app.get('/elements', (req, res) => {
+//   res.sendFile(__dirname + '/public/elements.html');
+// });
 
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/404.html');
