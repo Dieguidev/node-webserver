19dc92bde950cb497f1bff4f3ad9a1839aaa2fd3
Dieguidev <diegogaraycullas@gmail.com>
2024-02-15 12:42:05 -0500

    creados archivos html para servir contenido estatico

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+//servir contenido estatico
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(port, () => {
